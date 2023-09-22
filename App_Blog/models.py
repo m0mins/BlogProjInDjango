dcb667a9ecd907671caf26325739d11ec4e2d6fe
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-09-22 16:44:38 +0600

    comment ,blo create,read more done

--- App_Blog/models.py
+++ App_Blog/models.py
@@ -12,6 +12,9 @@ class Blog(models.Model):
     publish_date=models.DateTimeField(auto_now_add=True)
     update_date=models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering=('-publish_date',)
+
     def __str__(self):
         return self.blog_title
 
@@ -20,6 +23,8 @@ class Comment(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='user_comment')
     comment=models.TextField()
     comment_date=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering=('-comment_date',)
 
     def __str__(self):
         return self.comment
@@ -27,6 +32,10 @@ class Comment(models.Model):
 class Likes(models.Model):
     blog=models.ForeignKey(Blog,on_delete=models.CASCADE, related_name='liked_blog')
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='liker_user')
+
+
+    def __str__(self):
+        return self.user + "likes" +self.blog
 
 
 
